e163ff6ebb93b5f02e2ec060e2e723642153628e
Adolfo Neto <adolfo.usp@gmail.com>
2020-09-08 18:45:47 -0300

    Negated Atomic formulas added

--- lib/core/formula.ex
+++ lib/core/formula.ex
@@ -2,4 +2,8 @@ defmodule Logic.Formula do
   def new(atom) when is_atom(atom) do
     atom
   end
+
+  def new(:not, atom) when is_atom(atom) do
+    {:not, atom}
+  end
 end
--- test/formulas_test.exs
+++ test/formulas_test.exs
@@ -5,4 +5,8 @@ defmodule FormulasTest do
   test "create a new atomic formula" do
     assert Formula.new(:p) == :p
   end
+
+  test "create a new negated atomic formula" do
+    assert Formula.new(:not, :p) == {:not, :p}
+  end
 end
