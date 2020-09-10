fd822d981bd77dbffc667903e84c62b4ec41f3e0
Adolfo Neto <adolfo.usp@gmail.com>
2020-09-10 15:42:49 -0300

    Valuation for complex formaulas was implemented.

--- lib/logic/valuation.ex
+++ lib/logic/valuation.ex
@@ -12,4 +12,44 @@ defmodule Logic.Valuation do
   def value(valuation, atom) when is_atom(atom) do
     Map.get(valuation, atom)
   end
+
+  def value(valuation, {:not, subformula}) do
+    cond do
+      value(valuation, subformula) == :t ->
+        :f
+
+      true ->
+        :t
+    end
+  end
+
+  def value(valuation, {left, :and, right}) do
+    cond do
+      value(valuation, left) == :t and value(valuation, right) == :t ->
+        :t
+
+      true ->
+        :f
+    end
+  end
+
+  def value(valuation, {left, :or, right}) do
+    cond do
+      value(valuation, left) == :f and value(valuation, right) == :f ->
+        :f
+
+      true ->
+        :t
+    end
+  end
+
+  def value(valuation, {left, :implies, right}) do
+    cond do
+      value(valuation, left) == :t and value(valuation, right) == :f ->
+        :f
+
+      true ->
+        :t
+    end
+  end
 end
--- test/logic/valuation_test.exs
+++ test/logic/valuation_test.exs
@@ -1,6 +1,7 @@
 defmodule ValuationTest do
   use ExUnit.Case
   alias Logic.Valuation
+  alias Logic.Formula
 
   test "create a valuation for :p with  :t" do
     v = Valuation.new([:p], :t)
@@ -21,5 +22,95 @@ defmodule ValuationTest do
 
     assert Valuation.value(v, :p) == :f
     assert Valuation.value(v, :q) == :t
+  end
+
+  # COMPLEX FORMULAS
+
+  # UNARY
+
+  test "create a valuation v such that v(p)=f  and obtain v(!p)=t" do
+    v = Valuation.new(p: :f)
+
+    formula = Formula.new(:p, :not)
+
+    assert Valuation.value(v, formula) == :t
+  end
+
+  test "create a valuation v such that v(p)=t  and obtain v(!p)=f" do
+    v = Valuation.new(p: :t)
+
+    formula = Formula.new(:p, :not)
+
+    assert Valuation.value(v, formula) == :f
+  end
+
+  # BINARY
+
+  test "create a valuation v such that v(p)=f and v(q)=t and obtain v(p&q)=f" do
+    v = Valuation.new(p: :f, q: :t)
+
+    p_and_q = Formula.new({:p, :q}, :and)
+
+    assert Valuation.value(v, p_and_q) == :f
+  end
+
+  test "create a valuation v such that v(p)=t and v(q)=t and obtain v(p&q)=t" do
+    v = Valuation.new(p: :t, q: :t)
+
+    p_and_q = Formula.new({:p, :q}, :and)
+
+    assert Valuation.value(v, p_and_q) == :t
+  end
+
+  test "create a valuation v such that v(p)=f and v(q)=f and obtain v(p|q)=f" do
+    v = Valuation.new(p: :f, q: :f)
+
+    formula = Formula.new({:p, :q}, :or)
+
+    assert Valuation.value(v, formula) == :f
+  end
+
+  test "create a valuation v such that v(p)=t and v(q)=f and obtain v(p|q)=t" do
+    v = Valuation.new(p: :t, q: :f)
+
+    formula = Formula.new({:p, :q}, :or)
+
+    assert Valuation.value(v, formula) == :t
+  end
+
+  test "create a valuation v such that v(p)=t and v(q)=f and obtain v(p->q)=f" do
+    v = Valuation.new(p: :t, q: :f)
+
+    formula = Formula.new({:p, :q}, :implies)
+
+    assert Valuation.value(v, formula) == :f
+  end
+
+  test "create a valuation v such that v(p)=f and v(q)=t and obtain v(p->q)=t" do
+    v = Valuation.new(p: :f, q: :t)
+
+    formula = Formula.new({:p, :q}, :implies)
+
+    assert Valuation.value(v, formula) == :t
+  end
+
+  # COMBINATION
+
+  test "create a valuation v such that v(p)=f and v(q)=t and obtain v(!(p->q))=f" do
+    v = Valuation.new(p: :f, q: :t)
+
+    formula = Formula.new({:p, :q}, :implies) |> Formula.new(:not)
+
+    assert Valuation.value(v, formula) == :f
+  end
+
+  test "create a valuation v such that v(p)=f and v(q)=f and obtain v(!p->!q))=t" do
+    v = Valuation.new(p: :f, q: :f)
+
+    formula =
+      {Formula.new(:p, :not), Formula.new(:q, :not)}
+      |> Formula.new(:implies)
+
+    assert Valuation.value(v, formula) == :t
   end
 end
