5752f2c473aab5017b48ba881ed94fb73e74f718
Adolfo Neto <adolfo.usp@gmail.com>
2020-09-18 19:42:30 -0300

    I started but did not finish the implementation of a simple proof check.

--- lib/ke/proof.ex
+++ lib/ke/proof.ex
@@ -9,24 +9,34 @@ defmodule KE.Proof do
     proof
   end
 
-  def apply(proof = %__MODULE__{}, _rule) do
+  def apply_rule(proof = %__MODULE__{}, _rule) do
     # Here the rule must be the PB rule
     # the rule is applied and a new proof is returned
     # Does the PB rule can always be applied?
     proof
   end
 
-  def apply(proof = %__MODULE__{}, _rule, _premise) do
+  def apply_rule(proof = %__MODULE__{}, _rule, _premise) do
     # One of of the one-premise rules can only be applied if
     # 1. the premise is part of the list of sfs
     # 2. (?) its conclusion(s) are not part of the branch
     proof
   end
 
-  def apply(proof = %__MODULE__{}, _rule, _premise1, _premise2) do
+  def apply_rule(proof = %__MODULE__{}, _rule, _premise1, _premise2) do
     # One of of the two-premise rules can only be applied if
     # 1. the premises are part of the list of sfs
     # 2. (?) its conclusion is not part of the branch
     proof
   end
+
+  def example_for_tests() do
+    f = {:T, {:not, :p}}
+    p = new([f])
+
+    apply_rule(p, &KE.Rules.t_not/1, f)
+    |> IO.inspect()
+  end
 end
+
+KE.Proof.example_for_tests()
